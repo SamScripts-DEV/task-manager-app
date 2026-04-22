ad794dd485c0928f449cff089f0c24cba45587d5
No fui yo, fue el codigo <robsamuel.cuti@epn.edu.ec>
2026-04-22 12:05:44 -0500

    feat(frontend): add IonicStorageModule to main.ts

--- frontend-tareas-priorizadas/src/main.ts
+++ frontend-tareas-priorizadas/src/main.ts
@@ -1,13 +1,15 @@
-import { provideZoneChangeDetection } from "@angular/core";
+import { importProvidersFrom, provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {IonicStorageModule} from "@ionic/storage-angular";
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    importProvidersFrom(IonicStorageModule.forRoot()),
     provideZoneChangeDetection(),{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
