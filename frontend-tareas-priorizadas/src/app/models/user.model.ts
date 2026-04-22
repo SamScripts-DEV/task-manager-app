5e51da202560b790b4eca2162646197ee2666173
No fui yo, fue el codigo <robsamuel.cuti@epn.edu.ec>
2026-04-22 10:47:59 -0500

    feat(frontend/models): add new user interfaces

--- frontend-tareas-priorizadas/src/app/models/user.model.ts
+++ frontend-tareas-priorizadas/src/app/models/user.model.ts
@@ -5,7 +5,7 @@ export interface User {
 }
 
 
-export interface LoginResponse {
+export interface AuthResponse {
     access_token: string;
     token_type: string;
     user: User;
@@ -13,7 +13,14 @@ export interface LoginResponse {
 
 
 
-export interface SignUpRequest {
+export interface LoginRequest {
     email: string;
     password: string
 }
+
+export interface SignupRequest {
+    email: string;
+    password: string;
+    password_confirmation: string;
+}
+
