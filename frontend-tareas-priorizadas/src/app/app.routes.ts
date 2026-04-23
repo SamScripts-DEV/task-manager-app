cbbf92dce69d74254b536034a2a6fa5d86eb2ea1
No fui yo, fue el codigo <robsamuel.cuti@epn.edu.ec>
2026-04-23 08:39:49 -0500

    feat(frontend/routing): add route paths and apply functional auth guard

--- frontend-tareas-priorizadas/src/app/app.routes.ts
+++ frontend-tareas-priorizadas/src/app/app.routes.ts
@@ -1,13 +1,51 @@
 import { Routes } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '@services/auth.services';
+import { Router } from '@angular/router';
+import { map, take } from 'rxjs';
+
+
+const authGuardFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.isAuthenticated.pipe(
+    take(1),
+    map(isAuth => isAuth ? true : router.parseUrl('/login'))
+  );
+};
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'login', 
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'login', 
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent),
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./pages/signup/signup.component').then(m => m.SignupComponent),
+  },
+  {
+    path: 'tasks',
+    canActivate: [authGuardFn], 
+    loadComponent: () => import('./pages/tasks/tasks.component').then(m => m.TasksComponent),
+  },
+  {
+    path: 'create-task',
+    canActivate: [authGuardFn],
+    loadComponent: () => import('./pages/create-task/create-task.component').then(m => m.CreateTaskComponent),
+  },
+  {
+    path: 'edit-task/:id',
+    canActivate: [authGuardFn],
+    loadComponent: () => import('./pages/create-task/create-task.component').then(m => m.CreateTaskComponent),
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
   },
 ];
