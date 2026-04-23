304df0e19f87c421f04c0209e3c684bffab2f504
No fui yo, fue el codigo <robsamuel.cuti@epn.edu.ec>
2026-04-23 08:40:26 -0500

    feat(frontend/core): configure provideHttpClient in main.ts

--- frontend-tareas-priorizadas/src/main.ts
+++ frontend-tareas-priorizadas/src/main.ts
@@ -4,6 +4,9 @@ import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } 
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import {IonicStorageModule} from "@ionic/storage-angular";
 
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthInterceptor } from "@app/services/http.interceptors";
+
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
@@ -12,6 +15,14 @@ bootstrapApplication(AppComponent, {
     importProvidersFrom(IonicStorageModule.forRoot()),
     provideZoneChangeDetection(),{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
+
+    provideHttpClient(withInterceptorsFromDi()), 
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass:AuthInterceptor,
+      multi: true
+    },
+
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
 });
