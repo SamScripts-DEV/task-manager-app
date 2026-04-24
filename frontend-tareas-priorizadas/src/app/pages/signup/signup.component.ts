e20cf746a26f7ac7501c8c335cabfbd9c2d9f41f
No fui yo, fue el codigo <robsamuel.cuti@epn.edu.ec>
2026-04-24 11:03:32 -0500

    feat(frontend/auth): add signup component

---
+++ frontend-tareas-priorizadas/src/app/pages/signup/signup.component.ts
@@ -0,0 +1,65 @@
+﻿import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavController } from '@ionic/angular/standalone';
+import {
+    IonHeader, IonToolbar, IonTitle, IonButtons, IonBackButton,
+    IonContent, IonCard, IonCardContent, IonItem, IonLabel,
+    IonInput, IonButton, IonSpinner, IonText, IonIcon
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { personAddOutline } from 'ionicons/icons';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.services';
+import { ChangeDetectorRef } from '@angular/core';
+
+@Component({
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss'],
+    standalone: true,
+    imports: [
+        CommonModule, ReactiveFormsModule,
+        IonHeader, IonToolbar, IonTitle, IonButtons, IonBackButton,
+        IonContent, IonCard, IonCardContent, IonItem, IonLabel,
+        IonInput, IonButton, IonSpinner, IonText, IonIcon
+    ],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SignupComponent {
+    private fb = inject(FormBuilder);
+    private authService = inject(AuthService);
+    private navCtrl = inject(NavController);
+    private cdr = inject(ChangeDetectorRef);
+
+    signupForm: FormGroup = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        passwordConfirm: ['', [Validators.required]]
+    });
+
+    isLoading$ = this.authService.isLoading$;
+    error$ = this.authService.error$;
+
+
+    constructor() {
+        addIcons({ personAddOutline });
+    }
+
+    onSubmit() {
+        if (this.signupForm.valid) {
+            const { email, password, passwordConfirm } = this.signupForm.value;
+            this.authService.signup(email, password, passwordConfirm).subscribe({
+                next: () => {
+                    this.navCtrl.navigateRoot('/login');
+                },
+                error: (err) => {
+                    this.cdr.markForCheck();
+                }
+            });
+        }
+    }
+
+    goToLogin() {
+        this.navCtrl.navigateBack('/login');
+    }
+}
